58ed7d592cb3d30332e83aa8ba43116a8c457feb
tuannq98 <tuannq@ominext.com>
2020-05-07 22:45:36 +0700

    new project

--- CosmeticShop/resources/views/admin/home.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('admin_layout')
-@section('admin_content')
-    <h1>Chào</h1>
-@endsection
--- CosmeticShop/storage/framework/views/3a7d115c1859ceb9c116a575f5d4e8fdaa9fc486.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('admin_content'); ?>
-    <h1>Chào</h1>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('admin_layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\CosmeticShop\resources\views/admin/home.blade.php ENDPATH**/ ?>
